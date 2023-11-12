3815480886cc593ab7bf45a49fdba8d65dc43566
Your Name <you@example.com>
2023-11-12 18:31:26 +0800

    studentmenuui shit

--- app/src/main/java/cams/ui/StudentMenuUI.java
+++ app/src/main/java/cams/ui/StudentMenuUI.java
@@ -42,7 +42,7 @@ public class StudentMenuUI extends BaseUI{
                 break;
             case 2:
                 this.studentRegisterForCamp();
-                return 1;
+                break;
             case 3:
                 this.studentWithdrawFromCamp();
                 break;
@@ -69,19 +69,6 @@ public class StudentMenuUI extends BaseUI{
         }
         return 0;
     }
-
-
-
-    private void studentRegisterForCamp(){
-
-        System.out.println("Choose the Camp you want to join: ");
-    }
-
-
-    private void studentWithdrawFromCamp(){
-
-    }
-
 
     private void studentViewAllCamps() {
         System.out.println("=== All Available Camps ===");
@@ -116,11 +103,131 @@ public class StudentMenuUI extends BaseUI{
             }
         }
     }
+
+    private void studentRegisterForCamp() {
+        System.out.println("Choose the Camp you want to join:");
+    
+        // Display available camps with numbers
+        int index = 1;
+        for (Camp camp : MainApp.camps) {
+            System.out.println(index + ") " + camp.getCampName());
+            index++;
+        }
+    
+        // Get the camp number from the user
+        int selectedCampNumber = ScannerHelper.getIntegerInput("Enter the Camp Number to join (0 to cancel): ");
+    
+        if (selectedCampNumber == 0) {
+            System.out.println("Registration canceled.");
+            return;
+        }
+    
+        // Check if the selected camp number is within the valid range
+        if (selectedCampNumber >= 1 && selectedCampNumber <= MainApp.camps.size()) {
+            // Find the selected camp using the number
+            Camp selectedCamp = MainApp.camps.get(selectedCampNumber - 1);
+    
+            // Check if the student is already registered for the camp
+            if (selectedCamp.getListOfAttendees().contains(MainApp.currentUser.getUserID())) {
+                System.out.println("You are already registered for this camp.");
+            } else {
+                // Check if there are available spots
+                if (selectedCamp.getCampTotalSlots() > 0) {
+                    // Register the student for the camp
+                    selectedCamp.getListOfAttendees().add(MainApp.currentUser.getUserID());
+                    System.out.println("Registration successful for " + selectedCamp.getCampName() + ".");
+                } else {
+                    System.out.println("Sorry, no available spots for this camp.");
+                }
+            }
+        } else {
+            System.out.println("Invalid Camp Number. Please enter a valid number.");
+        }
+    }
+    
+    
+    private void studentWithdrawFromCamp() {
+        System.out.println("=== Your Registered Camps ===");
+    
+        // Display camps the student is registered for with numbers
+        int index = 1;
+        for (Camp camp : MainApp.camps) {
+            if (camp.getListOfAttendees().contains(MainApp.currentUser.getUserID())) {
+                System.out.println(index + ") " + camp.getCampName());
+                index++;
+            }
+        }
+    
+        // Get the camp number from the user
+        int selectedCampNumber = ScannerHelper.getIntegerInput("Enter the Camp Number to withdraw from (0 to cancel): ");
+    
+        if (selectedCampNumber == 0) {
+            System.out.println("Withdrawal canceled.");
+            return;
+        }
+    
+        // Check if the selected camp number is within the valid range
+        if (selectedCampNumber >= 1 && selectedCampNumber <= index - 1) {
+            // Find the selected camp using the number
+            Camp selectedCamp = getRegisteredCampByNumber(selectedCampNumber);
+    
+            // Check if the student is registered for the camp (extra check for safety)
+            if (selectedCamp != null && selectedCamp.getListOfAttendees().contains(MainApp.currentUser.getUserID())) {
+                // Withdraw the student from the camp
+                selectedCamp.getListOfAttendees().remove(MainApp.currentUser.getUserID());
+                System.out.println("Withdrawal successful from " + selectedCamp.getCampName() + ".");
+            } else {
+                System.out.println("You are not registered for this camp.");
+            }
+        } else {
+            System.out.println("Invalid Camp Number. Please enter a valid number.");
+        }
+    }
+    
+    private Camp getRegisteredCampByNumber(int number) {
+        int count = 0;
+        for (Camp camp : MainApp.camps) {
+            if (camp.getListOfAttendees().contains(MainApp.currentUser.getUserID())) {
+                count++;
+                if (count == number) {
+                    return camp;
+                }
+            }
+        }
+        return null;
+    }
+    
     
 
-
-    private void studentRegisteredCamps(){
-        
+    private void studentRegisteredCamps() {
+        System.out.println("=== Your Registered Camps ===");
+    
+        // Get the current student
+        Student currentStudent = (Student) MainApp.currentUser;
+    
+        // Create an ArrayList to store registered camps
+        ArrayList<Camp> registeredCamps = new ArrayList<>();
+    
+        // Loop through all camps
+        for (Camp camp : MainApp.camps) {
+            // Check if the current student is in the list of attendees
+            if (camp.getListOfAttendees().contains(currentStudent.getUserID())) {
+                registeredCamps.add(camp);
+            }
+        }
+    
+        // Display the registered camps
+        if (registeredCamps.isEmpty()) {
+            System.out.println("You haven't registered for any camps.");
+        } else {
+            for (Camp camp : registeredCamps) {
+                System.out.println("Camp ID: " + MainApp.uniqueID.getNextCampID());
+                System.out.println("Camp Name: " + camp.getCampName());
+                System.out.println("Start Date: " + camp.getStartDate());
+                System.out.println("End Date: " + camp.getEndDate());
+                System.out.println("=================");
+            }
+        }
     }
 
 
